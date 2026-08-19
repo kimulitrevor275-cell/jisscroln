9e436a105fcf44247bd7d33b870df2a137ffd8c3
Kimuli Trevor <kimulitrevor257@gmail.com>
2026-08-19 14:13:20 +0200

    badge

--- auth.js
+++ auth.js
@@ -166,10 +166,7 @@ var userId = window._cachedUserId||null;
     method : 'POST',
     headers: { 'Content-Type': 'application/json' },
     body   : JSON.stringify({ 
-      post_id: postId, 
-      text: text, 
-      username: username, 
-      user_id: userId }),
+      post_id: postId, text: text, username: username, user_id: userId }),
   })
   .then(function(r) { return r.json(); })
   .then(function(data) {
