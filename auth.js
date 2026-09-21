d418a0e1c1087c38586709128082abf67e2c7471
Kimuli Trevor <kimulitrevor257@gmail.com>
2026-09-21 18:39:36 +0530

    profile v1

--- auth.js
+++ auth.js
@@ -69,7 +69,7 @@ sb.auth.getSession().then(function(r) {
       var badge = _makeBadge(data.tier, 35);
       if (el) {
         el.innerHTML = getGreeting() + ', ' + name + badge +
-          ' &nbsp; <a href="#" onclick="signOut()" style="color:gray;font-size:20px;">sign out</a>';
+          ' &nbsp; <a href="profile.html" style="color:gray;font-size:20px;">👨</a>';
       }
     })
     .catch(function() {});
